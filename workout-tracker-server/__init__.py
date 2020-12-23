808053ec64c958d60394d5488325711c2c632a3c
Saar Sayfan <saarsayfan@gmail.com>
2020-12-22 20:45:36 -0800

    server skeleton

---
+++ workout-tracker-server/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
